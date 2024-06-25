dfb880c6695e5e2fbbec2e5dbcdef79a87f4b321
McConfig <juldashev@gmail.com>
2024-06-25 14:01:28 +0700

    Дан список, упорядоченный по неубыванию элементов в нем. Определите, сколько в нем различных элементов.

---
+++ base/day02/task/07_list/02_Number_of_different_elements.py
@@ -0,0 +1,16 @@
+# Считываем список чисел
+numbers = list(map(int, input().split()))
+
+# Определяем количество различных элементов
+unique_count = len(set(numbers))
+
+# Выводим количество различных элементов
+print(unique_count)
+
+
+
+
+
+
+
+
